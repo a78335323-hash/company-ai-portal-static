190882928dc33a4ef93c53a17625ab079334f38e
a78335323-hash <a78335323@gmail.com>
2026-01-21 11:54:21 +0100

    Update login.js

--- api/login.js
+++ api/login.js
@@ -4,12 +4,12 @@ import jwt from "jsonwebtoken";
 const USERS = [
   {
     email: "luca@lucagri.it",
-    passwordHash: "$2a$10$N9qo8uLOickgx2ZMRZo5e.e1zjv9Gz4H6Cw1n8F0cY7Z0C7x5tG3m",
+    passwordHash: "$2a$10$6eoWBtfc6y5EpiMNU9Tr1eRuLCivU2TVtdUApksuXpCbbQYzjTSIG",
     company: "Lucagri"
   },
   {
     email: "mattia@agrichampion.it",
-    passwordHash: "$2a$10$7EqJtq98hPqEX7fNZaFWo.8vG1pX9rQnqz9s9O4E3j5zK7dF9yF2i",
+    passwordHash: "$2a$10$9zD5QxhAwPYLOHLSByx4ou./p6t80MPoMJHAVqRIsxWKC6EwU4q2O",
     company: "Agrichampion"
   }
 ];
